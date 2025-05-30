6b136ca370d24aa415d1f54bcc4b7666831a7c40
Ilham Putra Husada <hambosto@gmail.com>
2025-05-31 03:04:42 +0700

    feat: switch colorscheme to tokyonight
    Removes nord colorscheme and adds tokyonight.
    Also updates astroui.lua to use tokyonight.

--- lua/community.lua
+++ lua/community.lua
@@ -14,6 +14,5 @@ return {
   { import = "astrocommunity.utility.noice-nvim" },
   { import = "astrocommunity.recipes.vscode-icons" },
   { import = "astrocommunity.indent.snacks-indent-hlchunk" },
-  { import = "astrocommunity.colorscheme.nord-nvim" },
   -- import/override with your plugins folder
 }
